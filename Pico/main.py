244315cef42f8ee2565b3e2ab01559b8023606ec
Billamw <66533754+Billamw@users.noreply.github.com>
2023-10-20 14:35:59 +0200

    Update main.py

--- Pico/main.py
+++ Pico/main.py
@@ -1,66 +1,55 @@
+# import machine
+# import utime
+
+# step_pin = machine.Pin(14, machine.Pin.OUT)
+# dir_pin = machine.Pin(15, machine.Pin.OUT)
+
+# def step(direction, steps, delay):
+#     dir_pin.value(direction)
+#     for i in range(steps):
+#         step_pin.value(1)
+#         utime.sleep_us(delay)
+#         step_pin.value(0)
+#         utime.sleep_us(delay)
+
+# delay = 508 # initial delay
+# counter = 0
+
+# while True:
+#     #if counter % 5 == 0:
+#     #    delay += 50 # increase delay by 100 microseconds every 20 seconds
+#     step(1, 200, delay) # clockwise
+#     # step(0, 200, delay) # counterclockwise
+#     counter += 1
+
 from machine import Pin
 import utime
-import time
-import ntptime
-import machine
-import network
 
-step = Pin(14, Pin.OUT)
-dir  = Pin(14, Pin.OUT)
+# Define pin connections & motor's steps per revolution
+dirPin = Pin(15, Pin.OUT)
+stepPin = Pin(14, Pin.OUT)
+stepsPerRevolution = 200
 
-full_step_sequenz = [
-    [1, 0, 0, 0],
-    [0, 1, 0, 0],
-    [0, 0, 1, 0],
-    [0, 0, 0, 1]
-]
-led = machine.Pin("LED")
+# Set motor direction clockwise
+dirPin.value(1)
 
+# Spin motor slowly
+for x in range(stepsPerRevolution):
+    stepPin.value(1)
+    utime.sleep_us(2000)
+    stepPin.value(0)
+    utime.sleep_us(2000)
 
-# CET Time in a tuble like time.gmtime()
-# def cettime():
-#     try:
-#         ntptime.settime()
-#     except:
-#         pass
-    
-#     year = time.localtime()[0]       #get current year
-#     HHMarch   = time.mktime((year,3 ,(31-(int(5*year/4+4))%7),1,0,0,0,0,0)) #Time of March change to CEST
-#     HHOctober = time.mktime((year,10,(31-(int(5*year/4+1))%7),1,0,0,0,0,0)) #Time of October change to CET
-#     now=time.time()
-#     if now < HHMarch :               # we are before last sunday of march
-#         cet=time.localtime(now+3600) # CET:  UTC+1H
-#     elif now < HHOctober :           # we are before last sunday of october
-#         cet=time.localtime(now+7200) # CEST: UTC+2H
-#     else:                            # we are after last sunday of october
-#         cet=time.localtime(now+3600) # CET:  UTC+1H
-#     return(cet)
+utime.sleep(1)  # Wait a second
 
-# Wi-Fi-Verbindung herstellen
-wifi = network.WLAN(network.STA_IF)
-wifi.active(True)
-wifi.connect("Fachschaft IMP", "8clFBb:oR;Q/#scBBj")  # SSID und Passwort anpassen
+# Set motor direction counterclockwise
+dirPin.value(0)
 
-print("connecting...")
+# Spin motor quickly
+for x in range(stepsPerRevolution):
+    stepPin.value(1)
+    utime.sleep_us(1000)
+    stepPin.value(0)
+    utime.sleep_us(1000)
 
-while not wifi.isconnected():
-    pass
-
-print("connected")
-
-# Watchdog inizialisieren
-wtd = machine.WDT()
-
-start_timer_request = time.time()
-start_timer_reset = time.time()
-request_duration = 60 * 60 # 60 minutes
-reset_duration = 12 * 60 * 60 # 24 hours
-
-
-i = 0
-while i < 10:
-    step.value(1)  # Setzt den step auf HIGH
-    time.sleep(1)  # Wartet eine Sekunde
-    step.value(0)  # Setzt den step auf LOW
-    time.sleep(1)  # Wartet eine Sekunde
-    i = i + 1
+utime.sleep(1)  # Wait a second
