d94e68e024a2d870a9fc24f307893096a002495e
Billamw <66533754+Billamw@users.noreply.github.com>
2023-10-28 23:30:28 +0200

    update

---
+++ Pico/a4988nema.py
@@ -0,0 +1,116 @@
+# Import the system modules needed to run rpiMotorlib.py
+import sys
+import time
+from machine import Pin
+
+class A4988Nema(object):
+    """ Class to control a Nema bi-polar stepper motor with a A4988 also tested with DRV8825"""
+    def __init__(self, direction_pin, step_pin, mode_pins, motor_type="A4988"):
+        """ class init method 3 inputs
+        (1) direction type=int , help=Pin pin connected to DIR pin of IC
+        (2) step_pin type=int , help=Pin pin connected to STEP of IC
+        (3) mode_pins type=tuple of 3 ints, help=Pin pins connected to
+        Microstep Resolution pins MS1-MS3 of IC, can be set to (-1,-1,-1) to turn off
+        Pin resolution.
+        (4) motor_type type=string, help=Type of motor two options: A4988 or DRV8825
+        """
+        self.motor_type = motor_type
+        self.direction_pin = direction_pin
+        self.step_pin = step_pin
+
+        if mode_pins[0] != -1:
+            self.mode_pins = mode_pins
+        else:
+            self.mode_pins = False
+
+        self.stop_motor = False
+
+    def motor_stop(self):
+        """ Stop the motor """
+        self.stop_motor = True
+
+    def resolution_set(self, steptype):
+
+        resolution = {'Full': (0, 0, 0),
+                        'Half': (1, 0, 0),
+                        '1/4': (0, 1, 0),
+                        '1/8': (1, 1, 0),
+                        '1/16': (1, 1, 1)}
+
+        # error check stepmode
+        if steptype in resolution:
+            pass
+        else:
+            print("Error invalid steptype: {}".format(steptype))
+            quit()
+
+        if self.mode_pins != False:
+            Pin(self.mode_pins, resolution[steptype])
+
+    def motor_go(self, clockwise=False, steptype="Full",
+                 steps=200, stepdelay=.005, verbose=False, initdelay=.05):
+        """ motor_go,  moves stepper motor based on 6 inputs
+
+         (1) clockwise, type=bool default=False
+         help="Turn stepper counterclockwise"
+         (2) steptype, type=string , default=Full help= type of drive to
+         step motor 5 options
+            (Full, Half, 1/4, 1/8, 1/16) 1/32 for DRV8825 only 1/64 1/128 for LV8729 only
+         (3) steps, type=int, default=200, help=Number of steps sequence's
+         to execute. Default is one revolution , 200 in Full mode.
+         (4) stepdelay, type=float, default=0.05, help=Time to wait
+         (in seconds) between steps.
+         (5) verbose, type=bool  type=bool default=False
+         help="Write pin actions",
+         (6) initdelay, type=float, default=1mS, help= Intial delay after
+         Pin pins initialized but before motor is moved.
+
+        """
+        self.stop_motor = False
+        # setup Pin
+        Pin(self.direction_pin, Pin.OUT)
+        Pin(self.step_pin, Pin.OUT)
+        Pin(self.direction_pin, clockwise)
+        if self.mode_pins != False:
+            Pin(self.mode_pins, Pin.OUT)
+
+        try:
+            # dict resolution
+            self.resolution_set(steptype)
+            time.sleep(initdelay)
+
+            for i in range(steps):
+                if self.stop_motor:
+                    raise InterruptedError
+                else:
+                    Pin(self.step_pin, True)
+                    time.sleep(stepdelay)
+                    Pin(self.step_pin, False)
+                    time.sleep(stepdelay)
+                    if verbose:
+                        print("Steps count {}".format(i+1), end="\r", flush=True)
+
+        except KeyboardInterrupt:
+            print("User Keyboard Interrupt : RpiMotorLib:")
+        except Exception as motor_error:
+            print(sys.exc_info()[0])
+            print(motor_error)
+            print("RpiMotorLib  : Unexpected error:")
+        else:
+            # print report status
+            if verbose:
+                print("\nRpiMotorLib, Motor Run finished, Details:.\n")
+                print("Motor type = {}".format(self.motor_type))
+                print("Clockwise = {}".format(clockwise))
+                print("Step Type = {}".format(steptype))
+                print("Number of steps = {}".format(steps))
+                print("Step Delay = {}".format(stepdelay))
+                print("Intial delay = {}".format(initdelay))
+                
+        finally:
+            # cleanup
+            Pin(self.step_pin, False)
+            Pin(self.direction_pin, False)
+            if self.mode_pins != False:
+                for pin in self.mode_pins:
+                    Pin(pin, False)
