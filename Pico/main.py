8d33f35c532bbc432b3beae408f37703bc600890
Billamw <66533754+Billamw@users.noreply.github.com>
2023-11-03 10:29:55 +0100

    Update main.py

--- Pico/main.py
+++ Pico/main.py
@@ -20,66 +20,83 @@ print('connected')
 # Watchdog inizialisieren
 wtd = WDT()
 
-url = 'http://192.168.1.241:8000/'
-# Schalter an Pin GP14 konfigurieren
-
-step_pins  = [Pin(0, Pin.OUT)]
-dir_pins   = [Pin(1, Pin.OUT)]
-en_pins    = [Pin(2, Pin.OUT)]
-turn_count = [0]
-# step_pins  = [Pin(0, Pin.OUT), Pin(3, Pin.OUT), Pin(6, Pin.OUT), Pin( 9, Pin.OUT), Pin(12, Pin.OUT)]
-# dir_pins   = [Pin(1, Pin.OUT), Pin(4, Pin.OUT), Pin(7, Pin.OUT), Pin(10, Pin.OUT), Pin(13, Pin.OUT)]
-# en_pins    = [Pin(2, Pin.OUT), Pin(5, Pin.OUT), Pin(8, Pin.OUT), Pin(11, Pin.OUT), Pin(14, Pin.OUT)]
-# turn_count = [0, 0, 0, 0, 0]
-
 led = Pin("LED")
+
+url = 'http://192.168.1.241:8000/'
 
 FULL_TURN = 200
 MAX_TURN = 10 * FULL_TURN
 
-def turn_off_motors():
-    for pin in en_pins:
-        pin.high()
+step_pins  = [Pin(0, Pin.OUT)]
+dir_pins   = [Pin(1, Pin.OUT)]
+en_pins    = [Pin(2, Pin.OUT)]
+step_count = [0]
+MAX_TURNS = [10*FULL_TURN]
+# step_pins  = [Pin(0, Pin.OUT), Pin(3, Pin.OUT), Pin(6, Pin.OUT), Pin( 9, Pin.OUT), Pin(12, Pin.OUT)]
+# dir_pins   = [Pin(1, Pin.OUT), Pin(4, Pin.OUT), Pin(7, Pin.OUT), Pin(10, Pin.OUT), Pin(13, Pin.OUT)]
+# en_pins    = [Pin(2, Pin.OUT), Pin(5, Pin.OUT), Pin(8, Pin.OUT), Pin(11, Pin.OUT), Pin(14, Pin.OUT)]
+# step_count = [0, 0, 0, 0, 0]
 
-def turn_on_motors():
-    for pin in en_pins:
-        pin.low()
 
-def switch_dir(dir:int):
-    for pin in dir_pins:
-        pin.value(dir)
+def turn_on_motor(en_pin):
+    en_pin.low()
 
-# def step(steps, delay):
-#     for pin in step_pins:
-#         for i in range(steps):
-#             pin.value(1)
-#             time.sleep_ms(delay)
-#             pin.value(0)
-#             time.sleep_ms(delay)
+def turn_off_motor(en_pin):
+    en_pin.high()
 
-def turn(pin, turns, delay):
-    en_pins[step_pins.index(pin)].low()
+def switch_dir(dir_pins:Pin, dir:int):
+    dir_pins.value(dir)
+
+def step(step_pin, steps, delay_ms):
+    idx = step_pins.index(step_pin)
+    if idx not in range(0, len(step_pins)):
+        return
+    # enable Motor
+    turn_on_motor(en_pins[idx])
+    for i in range(int(steps)):
+        step_pin.high()
+        time.sleep_ms(delay_ms)
+        step_pin.low()
+        time.sleep_ms(delay_ms)
+        step_count[idx] += 1
+
+        if step_count[idx]*FULL_TURN >= MAX_TURNS[idx]:
+            return
+    # disable motor
+    turn_off_motor(en_pins[idx])
+
+def full_turn(step_pin, turns, delay_ms):
+    # getting index of step_pin in Step_pins array
+    idx = step_pins.index(step_pin)
+    if idx not in range(0, len(step_pins)):
+        return
+    # enable Motor
+    turn_on_motor(en_pins[idx])
     for i in range(int(turns * FULL_TURN)):
-        pin.value(1)
-        time.sleep_ms(delay)
-        pin.value(0)
-        time.sleep_ms(delay)
-        turn_count[step_pins.index(pin)] += 1
+        step_pin.high()
+        time.sleep_ms(delay_ms)
+        step_pin.low()
+        time.sleep_ms(delay_ms)
+        step_count[idx] += 1
 
-        for count in turn_count:
-            if count*FULL_TURN >= MAX_TURN:
-                break
-    print(turn_count)
+        if step_count[idx]*FULL_TURN >= MAX_TURNS[idx]:
+            return
+
+    # disable motor
+    turn_off_motor(en_pins[idx])
+
 
 print("initialized")
 
 
-turn(step_pins[0], 1, 2)
+full_turn(step_pins[0], 1, 2)
 en_pins[0].high()
-last_status = 'doorOpenn'
-status = ''
-
 print('steps done')
+
+
+# last_status = 'doorOpenn'
+# status = ''
+
 
 # while True:
 
