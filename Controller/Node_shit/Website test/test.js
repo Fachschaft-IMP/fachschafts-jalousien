3ac9737845a4a3b02a1c256c066593b6dae2a525
Billamw <66533754+Billamw@users.noreply.github.com>
2023-11-15 22:09:23 +0100

    update

---
+++ Controller/Node_shit/Website test/test.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+let volume = 0;
+
+// Use body-parser middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+// Serving static files from 'public' directory
+app.use(express.static('public'));
+
+// POST route to receive slider value
+app.post('/set-volume', (req, res) => {
+ volume = req.body.volume;
+ console.log('Received volume:', volume);
+ res.send('Volume received');
+});
+
+app.get('/get-volume', (req, res) => {
+  res.send({ volume: volume });
+ });
+
+app.listen(3000, () => {
+ console.log('Server started on port http://localhost:3000/');
+});
