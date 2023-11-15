714d9f5b30b785573f34899b1caef3621b65cdae
Billamw <66533754+Billamw@users.noreply.github.com>
2023-11-15 11:14:53 +0100

    update

--- Controller/TCP_Senden/run_node.js
+++ Controller/TCP_Senden/run_node.js
@@ -1,33 +1,30 @@
 const http = require('http');
 const fs = require('fs');
-var net = require('net');
+const url = require('url');
 
-const server = http.createServer((req, res) => {
-    fs.readFile('Controller/TCP_Senden/send.html', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            res.statusCode = 500;
-            res.end('Server Error');
-        } else {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end(data);
-        }
-    });
-
-    const bodyParser = require('body-parser');
-    server.use(bodyParser.urlencoded({ extended: true }));
-
-    server.post('/', (req, res) => {
-    console.log(req.body.value);
-    res.end('Received value: ' + req.body.value);
-});
+const server = http.createServer(function (req, res) {
+  const q = url.parse(req.url, true);
+  let filename = "." + q.pathname;
+  if (filename === './') {
+    filename = './Controller/TCP_Senden/send.html';
+  }
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }  
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    const slider1 = q.query.slider1;
+    const slider2 = q.query.slider2;
+    const slider3 = q.query.slider3;
+    const slider4 = q.query.slider4;
+    const slider5 = q.query.slider5;
+    console.log(`Slider 1: ${slider1}, Slider 2: ${slider2}, Slider 3: ${slider3}, Slider 4: ${slider4}, Slider 5: ${slider5}`);
+    return res.end();
+  });
 });
 
 server.listen(3000, '127.0.0.1', () => {
-   console.log('Server running at http://127.0.0.1:3000/');
-});
-
-
-
-
+    console.log('Server running at http://127.0.0.1:3000/');
+ });
