16629dbf78440d123c6037f6c9bc2974c22236fd
Billamw <66533754+Billamw@users.noreply.github.com>
2023-11-17 11:33:13 +0100

    update

---
+++ Controller/Frontend/vert_slider_angepasst/slider.js
@@ -0,0 +1,33 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const slider = document.getElementById('sss');
+//     const lamellen = document.querySelectorAll('.lamelle');
+
+//     const initialPosition = lamellen[0].offsetTop;
+
+
+//     slider.addEventListener('input', function() {
+//         const value = slider.value;
+//         let i = 0;
+//         for (const lamelle of lamellen) {
+//             lamelle.style.top = initialosition + (value - slider.min) * (i * lamelle.offsetHeight*0.8) / (slider.max - slider.min) + 'px';
+//             i++;
+//             console.log("test");
+//         }
+//     });
+// });
+
+const slider = document.getElementById('sss');
+const lamellen = document.querySelectorAll('.lamelle');
+
+const initialPosition = lamellen[0].offsetTop;
+
+
+slider.addEventListener('input', function() {
+    const value = slider.value;
+    let i = 0;
+    for (const lamelle of lamellen) {
+        lamelle.style.top = initialosition + (value - slider.min) * (i * lamelle.offsetHeight*0.8) / (slider.max - slider.min) + 'px';
+        i++;
+        console.log("test");
+    }
+});
