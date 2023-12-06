6b1cc415d1a83d143da829ce49de7b9955022cb5
MilanJzo <milan.jezovsek@gmail.com>
2023-12-06 10:35:30 +0100

    DIESER DUMME BORDERADIUS AUF DEN LAMMELLEN KOMMT JETZT WEG

--- Controller/Frontend/vert_slider_angepasst/slider.js
+++ Controller/Frontend/vert_slider_angepasst/slider.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 let i = 0;
                 for (const lamelle of lamellen) {
                     // lamelle.style.top = initialPosition + (value - slider.min) * (i * lamelle.offsetHeight * 0.8) / (slider.max - slider.min) + 'px';
-                    lamelle.style.top = initialPosition + value * (100 / lamellen.length * i * 0.988) + '%';
+                    lamelle.style.top = initialPosition + value * (100 / lamellen.length * i) + '%';
                     i++;
                 }
             });
