fa5073d4acd2b187149d218aeb7ed8ca0e0a633e
Billamw <66533754+Billamw@users.noreply.github.com>
2023-12-07 19:54:52 +0100

    update

--- Controller/Frontend/vert_slider_angepasst/slider.js
+++ Controller/Frontend/vert_slider_angepasst/slider.js
@@ -1,51 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
+const slider = document.getElementById('jalou-slider1');
+const lamellen = document.querySelectorAll('.lamelle');
 
-            const slider = document.getElementById('sss');
-            const lamellen = document.querySelectorAll('.lamelle');
+const initialPosition = lamellen[0].offsetTop;
 
-            const initialPosition = lamellen[0].offsetTop;
-
-            slider.addEventListener('input', function() {
-                const value = slider.value;
-                let i = 0;
-                for (const lamelle of lamellen) {
-                    // lamelle.style.top = initialPosition + (value - slider.min) * (i * lamelle.offsetHeight * 0.8) / (slider.max - slider.min) + 'px';
-                    lamelle.style.top = initialPosition + value * (100 / lamellen.length * i) + '%';
-                    i++;
-                }
-            });
-        });
-
-// // document.addEventListener('DOMContentLoaded', function() {
-// //     const slider = document.getElementById('sss');
-// //     const lamellen = document.querySelectorAll('.lamelle');
-
-// //     const initialPosition = lamellen[0].offsetTop;
-
-
-// //     slider.addEventListener('input', function() {
-// //         const value = slider.value;
-// //         let i = 0;
-// //         for (const lamelle of lamellen) {
-// //             lamelle.style.top = initialosition + (value - slider.min) * (i * lamelle.offsetHeight*0.8) / (slider.max - slider.min) + 'px';
-// //             i++;
-// //             console.log("test");
-// //         }
-// //     });
-// // });
-
-// const slider = document.getElementById('sss');
-// const lamellen = document.querySelectorAll('.lamelle');
-
-// const initialPosition = lamellen[0].offsetTop;
-
-
-// slider.addEventListener('input', function() {
-//     const value = slider.value;
-//     let i = 0;
-//     for (const lamelle of lamellen) {
-//         lamelle.style.top = initialosition + (value - slider.min) * (i * lamelle.offsetHeight*0.8) / (slider.max - slider.min) + 'px';
-//         i++;
-//         console.log("test");
-//     }
-// });
+slider.addEventListener('input', function() {
+    const value = slider.value;
+    let i = 0;
+    for (const lamelle of lamellen) {
+        // lamelle.style.top = initialPosition + (value - slider.min) * (i * lamelle.offsetHeight * 0.8) / (slider.max - slider.min) + 'px';
+        lamelle.style.top = initialPosition + value * (100 / lamellen.length * i) + '%';
+        i++;
+    }
+});
