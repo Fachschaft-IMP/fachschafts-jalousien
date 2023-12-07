fcbf27588f6e74037e6491b0584052f40659fb78
Billamw <66533754+Billamw@users.noreply.github.com>
2023-12-07 21:12:19 +0100

    update

--- Controller/Frontend/vert_slider_angepasst/slider.js
+++ Controller/Frontend/vert_slider_angepasst/slider.js
@@ -1,14 +1,49 @@
-const slider = document.getElementById('jalou-slider1');
+const slider1 = document.getElementById('jalou-slider1');
+const slider2 = document.getElementById('jalou-slider2');
+const slider3 = document.getElementById('jalou-slider3');
+const slider4 = document.getElementById('jalou-slider4');
+const slider5 = document.getElementById('jalou-slider5');
 const lamellen = document.querySelectorAll('.lamelle');
+const lamPair = lamellen.length/5
 
 const initialPosition = lamellen[0].offsetTop;
 
-slider.addEventListener('input', function() {
-    const value = slider.value;
+slider1.addEventListener('input', function() {
+    const value = slider1.value;
     let i = 0;
-    for (const lamelle of lamellen) {
-        // lamelle.style.top = initialPosition + (value - slider.min) * (i * lamelle.offsetHeight * 0.8) / (slider.max - slider.min) + 'px';
-        lamelle.style.top = initialPosition + value * (100 / lamellen.length * i) + '%';
-        i++;
+    for(let j=0; j< lamPair; j++){
+        lamellen[j].style.top = initialPosition + value * (100 / lamPair * j) + '%';
+    }
+});
+
+slider2.addEventListener('input', function() {
+    const value = slider2.value;
+    let i = 0;
+    for(let j=0; j< lamPair; j++){
+        lamellen[j+lamPair].style.top = initialPosition + value * (100 / lamPair * j) + '%';
+    }
+});
+
+slider3.addEventListener('input', function() {
+    const value = slider3.value;
+    let i = 0;
+    for(let j=0; j< lamPair; j++){
+        lamellen[j+2*lamPair].style.top = initialPosition + value * (100 / lamPair * j) + '%';
+    }
+});
+
+slider4.addEventListener('input', function() {
+    const value = slider4.value;
+    let i = 0;
+    for(let j=0; j< lamPair; j++){
+        lamellen[j+3*lamPair].style.top = initialPosition + value * (100 / lamPair * j) + '%';
+    }
+});
+
+slider5.addEventListener('input', function() {
+    const value = slider5.value;
+    let i = 0;
+    for(let j=0; j< lamPair; j++){
+        lamellen[j+4*lamPair].style.top = initialPosition + value * (100 / lamPair * j) + '%';
     }
 });
