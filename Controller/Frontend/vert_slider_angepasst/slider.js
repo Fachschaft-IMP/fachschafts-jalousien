c23232b9a8651b2538c7c0623cbec2c62d9378f7
daFruit <milan.jezovsek@gmail.com>
2023-12-14 00:02:32 +0100

    styling nightmare

--- Controller/Frontend/vert_slider_angepasst/slider.js
+++ Controller/Frontend/vert_slider_angepasst/slider.js
@@ -3,6 +3,9 @@ const slider2 = document.getElementById('jalou-slider2');
 const slider3 = document.getElementById('jalou-slider3');
 const slider4 = document.getElementById('jalou-slider4');
 const slider5 = document.getElementById('jalou-slider5');
+
+const master = document.getElementById('master-slider');
+
 const lamellen = document.querySelectorAll('.lamelle');
 const lamPair = lamellen.length/5
 
@@ -45,5 +48,21 @@ slider5.addEventListener('input', function() {
     let i = 0;
     for(let j=0; j< lamPair; j++){
         lamellen[j+4*lamPair].style.top = initialPosition + value * (100 / lamPair * j) + '%';
+    }
+});
+
+
+master.addEventListener('input', function() {
+    const value = master.value;
+    slider1.value = value;
+    slider2.value = value;
+    slider3.value = value;
+    slider4.value = value;
+    slider5.value = value;
+
+    for (let window = 0; window < 5; window++) {
+        for (let lamelle = 0; lamelle < 16; lamelle++) {
+            lamellen[window*16 + lamelle].style.top = initialPosition + value * (100 / 16 * lamelle) + '%';
+        }
     }
 });
